cbb9b46fed8a8a78dfabd5961b0fcf7256b7ca72
Macartur Sousa <macartur.sc@gmail.com>
2016-05-20 10:40:44 -0300

    Fixed CommunityAssociation
    Signed-off-by: Macartur Sousa <macartur.sc@gmail.com>

--- src/colab-spb-plugin/src/colab_spb/models.py
+++ src/colab-spb-plugin/src/colab_spb/models.py
@@ -5,11 +5,15 @@ from django.db import models
 
 
 class CommunityAssociations(models.Model):
-    community = models.ForeignKey(noosfero.NoosferoCommunity, default=1)
-    group = models.ForeignKey(gitlab.GitlabGroup, default=1)
-    mail_list = models.ForeignKey(mailman.MailingList, default=1)
+    community = models.ForeignKey(noosfero.NoosferoCommunity, null=True)
+    group = models.ForeignKey(gitlab.GitlabGroup, null=True)
+    mail_list = models.ForeignKey(mailman.MailingList, null=True)
 
     def __unicode__(self):
+        if self.community is None or self.group.name is None \
+           or self.mail_list.name is None:
+            return u'Invalid CommunityAssociation'
+
         return u'Social: {} - Dev: {} - List: {}'.format(self.community.name,
                                                          self.group.name,
                                                          self.mail_list.name)
